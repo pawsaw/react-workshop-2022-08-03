0d7f3c2c6e58bdbca5b4de296949c6561d3cc172
Pawel Sawicki <public@psawicki.de>
2022-08-08 10:19:36 +0200

    useBooks - custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import './App.css';
-import { BookDetail } from './components/BookDetail';
+import { BookList } from './components/BookList';
+import { useBooks } from './domain/books';
 
 function App() {
+  // const [book, setBook] = useState<Book | null>(null);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch(`http://localhost:4730/books/1001606140805`);
+  //     const _book = await response.json();
+  //     setBook(_book);
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+  const { books, reload } = useBooks();
+
   return (
     <div className="App">
-      {/* <BookList books={book} onBookSelected={onBookSelected} /> */}
-      <BookDetail />
+      {books ? <BookList books={books} /> : <span>Loading books...</span>}
+      <button onClick={reload}>Reload Books</button>
+      {/* {book ? <BookDetail book={book} /> : <span>Loading</span>} */}
     </div>
   );
 }
--- src/components/BookDetail/BookDetail.tsx
+++ src/components/BookDetail/BookDetail.tsx
@@ -1,32 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Book } from '../../domain/books';
 
-export interface BookDetailProps {}
+export interface BookDetailProps {
+  book: Book;
+}
 
-export const BookDetail: React.FC<BookDetailProps> = () => {
-  const [book, setBook] = useState<Book | null>(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`http://localhost:4730/books/1001606140805`);
-      const _book = await response.json();
-      setBook(_book);
-    };
-
-    fetchData();
-  }, []);
-
+export const BookDetail: React.FC<BookDetailProps> = ({ book }) => {
   return (
     <div>
-      {book ? (
-        <div>
-          <h2>
-            Book with ISBN: {book.isbn} - {book.title}
-          </h2>
-          <p>{book.abstract}</p>
-        </div>
-      ) : (
-        <span>Loading...</span>
-      )}
+      <h2>
+        Book with ISBN: {book.isbn} - {book.title}
+      </h2>
+      <p>{book.abstract}</p>
     </div>
   );
 };
